0d8807ab4009b64b8ae6c6bf862c50fc3c92d170
Nadif <nadiefulfi243@gmail.com>
2026-03-01 01:30:06 +0700

    chore: revise Yuura's channelId

--- database/seed_data.sql
+++ database/seed_data.sql
@@ -3,12 +3,11 @@
 -- ============================================================================
 
 -- Yuura's basic info
--- TODO: Replace placeholder values with actual data from her channel
-INSERT INTO talents (name, handle, youtube_id, debut_date, description)
+INSERT INTO talents (name, handle, youtube_id, debut_date, agency, description)
 VALUES (
     'Yuura Yozakura',
     '@YuuraYozakura',
-    'UC0ZYul2i5OcyKbdKB2v1O2w',-- externalId: UCrmtSeYObZJNG6OIC7Pw-_A
+    'UCrmtSeYObZJNG6OIC7Pw-_A',
     '2022-09-27',
     'おや、よく来たの。わしは夜桜優空じゃ。以後、お見知り置きよ! よこそ、夜桜旅館へ！
     Selamat datang di penginapanku semua, namaku Yuura！ Senang bertemu kalian！'
